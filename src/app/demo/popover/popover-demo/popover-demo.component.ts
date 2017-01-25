5120d439a4c4d198e6e0e8e4a73d618164162567
kmpeach <kevin@interfacewerk.de>
2017-01-25 23:28:42 +0100

    yarn and styles

--- src/app/demo/popover/popover-demo/popover-demo.component.ts
+++ src/app/demo/popover/popover-demo/popover-demo.component.ts
@@ -6,19 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./popover-demo.component.scss']
 })
 export class PopoverDemoComponent implements OnInit {
-
-  isPopoverOpen1 = true;
-  myText = 'some text here';
-  trucs: {isOpen: boolean}[] = [];
+  editedPerson: Person;
+  editedField: string;
+  persons: Person[] = [];
 
   constructor() {
     for(let i = 0; i < 100; i++) {
-      this.trucs.push({isOpen: false});
+      this.persons.push({
+        first: this.firsts[Math.floor(Math.random() * this.firsts.length)],
+        last: this.lasts[Math.floor(Math.random() * this.lasts.length)],
+      });
     }
   }
 
   ngOnInit() {
   }
 
+  firsts = ['Mark', 'Edy', 'Jack', 'John', 'Mike'];
+  lasts = ['Johnson', 'Willis', 'Chapman', 'Bob', 'Last'];
+}
 
+type Person = {
+  first: string
+  last: string
 }
