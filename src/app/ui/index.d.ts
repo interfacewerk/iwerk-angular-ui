3b0492f71e6adcc9efc5835d071b9a3fa119d460
Kevin Merckx <kevin@interfacewerk.de>
2017-09-16 13:41:10 +0200

    lint test 1

---
+++ src/app/ui/index.d.ts
@@ -0,0 +1,11 @@
+declare namespace IWUi {
+  type PopoverHorizontalAlignment = 'leftWithLeft' |  undefined;  
+  
+  interface IPopover {
+    close: () => void;
+  }
+
+  interface PopoverPosition {
+    vertical: 'top' | 'bottom';
+  }
+}
