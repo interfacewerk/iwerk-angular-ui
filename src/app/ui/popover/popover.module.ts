155df246008843145fd9542fd03bd15a4beb8976
kevinmerckx <kevinmerckx@users.noreply.github.com>
2017-09-16 14:19:09 +0200

    ci(lint): enable tslinter in ci (#12)

--- src/app/ui/popover/popover.module.ts
+++ src/app/ui/popover/popover.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PopoverDirective, PopoverPosition, PopoverTargetDirective } from './popover.directive';
+import { PopoverDirective, PopoverTargetDirective } from './popover.directive';
 import { PopoverContainerComponent } from './popover-container/popover-container.component';
 import { PopoverScrollMaskComponent } from './popover-scroll-mask/popover-scroll-mask.component';
-import { PopoverService, IPopover, Popover } from './popover.service';
-export { PopoverPosition, PopoverService, IPopover, Popover };
+import { PopoverService, Popover } from './popover.service';
+export { PopoverService, Popover };
 
 @NgModule({
   entryComponents: [ PopoverContainerComponent, PopoverScrollMaskComponent ],
   imports: [
     CommonModule
   ],
-  declarations: [ PopoverDirective, PopoverContainerComponent, PopoverScrollMaskComponent, PopoverTargetDirective ],
+  declarations: [
+    PopoverDirective,
+    PopoverContainerComponent,
+    PopoverScrollMaskComponent,
+    PopoverTargetDirective
+  ],
   exports: [ PopoverDirective, PopoverTargetDirective ],
   providers: [ PopoverService ]
 })
