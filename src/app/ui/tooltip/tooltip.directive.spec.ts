2fa920d0f06b7e91c6c5cb907fd1aba8b557e85e
Kevin Merckx <kevin-github@interfacewerk.de>
2017-10-06 20:12:10 +0200

    test(tooltip): add basic tests

--- src/app/ui/tooltip/tooltip.directive.spec.ts
+++ src/app/ui/tooltip/tooltip.directive.spec.ts
@@ -1,26 +1,57 @@
-
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component, NgModule } from '@angular/core';
 import { TooltipDirective } from './tooltip.directive';
+import { TooltipContainerComponent } from './tooltip-container/tooltip-container.component';
+
+@Component({
+  template: '<ng-template iwTooltip>Here is my tooltip</ng-template>'
+})
+class TestComponent {
+
+}
+
+@NgModule({
+  declarations: [TooltipContainerComponent],
+  entryComponents: [
+    TooltipContainerComponent,
+  ]
+})
+class TestModule {}
 
 describe('TooltipDirective', () => {
-  let directive: TooltipDirective;
-  let fixture: ComponentFixture<TooltipDirective>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ TooltipDirective ]
-    })
-    .compileComponents();
-  }));
+  let component: TestComponent;
+  let fixture: ComponentFixture<TestComponent>;
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(TooltipDirective);
-    directive = fixture.componentInstance;
+    TestBed.configureTestingModule({
+      declarations: [ TestComponent, TooltipDirective ],
+      imports: [TestModule]
+    });
+    fixture = TestBed.createComponent(TestComponent);
+    component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('appends the tooltip to the body when the mouse enters the element', () => {
+    (<HTMLElement>(fixture.nativeElement)).dispatchEvent(new MouseEvent('mouseenter'));
+    expect(document.body.querySelectorAll('iw-tooltip-container').length).toBe(1);
+  });
+
+  it('removes the tooltip when the mouse enters and leaves the element', () => {
+    (<HTMLElement>(fixture.nativeElement)).dispatchEvent(new MouseEvent('mouseenter'));
+    (<HTMLElement>(fixture.nativeElement)).dispatchEvent(new MouseEvent('mouseleave'));
+    expect(document.body.querySelectorAll('iw-tooltip-container').length).toBe(0);
+  });
+
+  it('removes the tooltip when the mouse enters and the directive is destroyed', () => {
+    (<HTMLElement>(fixture.nativeElement)).dispatchEvent(new MouseEvent('mouseenter'));
+    expect(document.body.querySelectorAll('iw-tooltip-container').length).toBe(1);
+    fixture.destroy();
+    expect(document.body.querySelectorAll('iw-tooltip-container').length).toBe(0);
+  });
+
+  it('projects the content', () => {
+    (<HTMLElement>(fixture.nativeElement)).dispatchEvent(new MouseEvent('mouseenter'));
+    expect(document.body.querySelectorAll('iw-tooltip-container').item(0).textContent).toBe('Here is my tooltip');
+  });
 });
