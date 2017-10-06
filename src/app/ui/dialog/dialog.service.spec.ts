96182317a53cb0729ac84b0a2a1f58b9e85f5f68
Kevin Merckx <kevin-github@interfacewerk.de>
2017-10-06 23:44:34 +0200

    test(dialog.service): add tests to dialog service

--- src/app/ui/dialog/dialog.service.spec.ts
+++ src/app/ui/dialog/dialog.service.spec.ts
@@ -1,13 +1,13 @@
 import { DialogService } from './dialog.service';
 import { DialogContainerComponent } from './dialog-container/dialog-container.component';
 import { TestBed } from '@angular/core/testing';
-import { Component, NgModule } from '@angular/core';
+import { Component, NgModule, ApplicationRef } from '@angular/core';
 
 @Component({
-  template: 'some dialog'
+  template: 'some dialog {{someVariable}}'
 })
 class DialogServiceTestComponent {
-
+  someVariable: string;
 }
 
 @NgModule({
@@ -30,7 +30,34 @@ describe('dialog.service', () => {
     dialogService = TestBed.get(DialogService);
   });
 
-  it('should not throw an error when no option is provided', () => {
-    expect(() => dialogService.open(DialogServiceTestComponent)).not.toThrow();
+  it('appends a container to the body and is able to remove it', () => {
+    const dialog = dialogService.open(DialogServiceTestComponent);
+    expect(document.body.querySelectorAll('iw-dialog-container').length).toBe(1);
+    dialog.close();
+    expect(document.body.querySelectorAll('iw-dialog-container').length).toBe(0);
   });
+
+  it('only opens one dialog at a time', () => {
+    dialogService.open(DialogServiceTestComponent);
+    dialogService.open(DialogServiceTestComponent);
+    expect(document.body.querySelectorAll('iw-dialog-container').length).toBe(1);
+    dialogService.close();
+    expect(document.body.querySelectorAll('iw-dialog-container').length).toBe(0);
+  });
+
+  it('does not throw an error when no option is provided', () => {
+    expect(() => dialogService.open(DialogServiceTestComponent)).not.toThrow();
+    dialogService.close();
+  });
+
+  it('initializes the members of the projected content', () => {
+    dialogService.open(DialogServiceTestComponent, undefined, {
+      someVariable: 'here it is'
+    });
+    const appRef: ApplicationRef = TestBed.get(ApplicationRef);
+    appRef.tick();
+    expect(document.body.querySelectorAll('iw-dialog-container').item(0).textContent.trim()).toBe('some dialog here it is');
+    dialogService.close();
+  });
+
 });
