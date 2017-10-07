5c75b2b676a993e4fdd0507988143b57b6769021
Kevin Merckx <kevin-github@interfacewerk.de>
2017-10-07 13:06:56 +0200

    test(dialog-container): add tests to dialog-container component

--- src/app/ui/dialog/dialog-container/dialog-container.component.spec.ts
+++ src/app/ui/dialog/dialog-container/dialog-container.component.spec.ts
@@ -17,12 +17,67 @@ describe('DialogContainerComponent', () => {
     fixture = TestBed.createComponent(DialogContainerComponent);
     component = fixture.componentInstance;
     component.dialogOptions = {
-      containerClass: ''
+      containerClass: 'class1 class2'
     };
     fixture.detectChanges();
   });
 
-  it('should be created', () => {
-    expect(component).toBeTruthy();
+  it('emits an onClose event if user clicks on it and clickToClose=true', () => {
+    const obj = {
+      toCall: () => {}
+    };
+    component.dialogOptions = {
+      clickToClose: true
+    };
+    component.onClose.subscribe(spyOn(obj, 'toCall'));
+    (<HTMLElement>(fixture.nativeElement)).dispatchEvent(new MouseEvent('click'));
+    expect(obj.toCall).toHaveBeenCalled();
+  });
+
+  it('does not emit an onClose event if user clicks on it and clickToClose=false', () => {
+    const obj = {
+      toCall: () => {}
+    };
+    component.dialogOptions = {
+      clickToClose: false
+    };
+    component.onClose.subscribe(spyOn(obj, 'toCall'));
+    (<HTMLElement>(fixture.nativeElement)).dispatchEvent(new MouseEvent('click'));
+    expect(obj.toCall).not.toHaveBeenCalled();
+  });
+
+  it('emits an onClose event if user presses ESC and escToClose=true', () => {
+    const obj = {
+      toCall: () => {}
+    };
+    component.dialogOptions = {
+      escToClose: true
+    };
+    component.onClose.subscribe(spyOn(obj, 'toCall'));
+    const event = document.createEvent('Event');
+    (<{keyCode: number}><any>event).keyCode = 27;
+    event.initEvent('keydown', true, true);
+    window.dispatchEvent(event);
+    expect(obj.toCall).toHaveBeenCalled();
+  });
+
+  it('does not emit an onClose event if user presses ESC and escToClose=false', () => {
+    const obj = {
+      toCall: () => {}
+    };
+    component.dialogOptions = {
+      escToClose: false
+    };
+    component.onClose.subscribe(spyOn(obj, 'toCall'));
+    const event = document.createEvent('Event');
+    (<{keyCode: number}><any>event).keyCode = 27;
+    event.initEvent('keydown', true, true);
+    window.dispatchEvent(event);
+    expect(obj.toCall).not.toHaveBeenCalled();
+  });
+
+  it('adds classes passed with dialogOptions', () => {
+    expect(fixture.debugElement.classes['class1']).toBe(true);
+    expect(fixture.debugElement.classes['class2']).toBe(true);
   });
 });
