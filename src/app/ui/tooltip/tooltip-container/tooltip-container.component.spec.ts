efcc484fcf0cb86cb74909fb388c2ff661ca63c2
Kevin Merckx <kevin-github@interfacewerk.de>
2017-10-07 13:26:57 +0200

    test(tooltip-container): add basic test

--- src/app/ui/tooltip/tooltip-container/tooltip-container.component.spec.ts
+++ src/app/ui/tooltip/tooltip-container/tooltip-container.component.spec.ts
@@ -1,7 +1,12 @@
 
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { TooltipContainerComponent } from './tooltip-container.component';
+import { IW_TOOLTIP_CONFIG } from '../tooltip.config';
+import { TooltipConfig } from '../tooltip-config.interface';
+
+const config: TooltipConfig = {
+  containerClass: 'class-in-config'
+};
 
 describe('TooltipContainerComponent', () => {
   let component: TooltipContainerComponent;
@@ -9,18 +14,26 @@ describe('TooltipContainerComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TooltipContainerComponent ]
+      declarations: [TooltipContainerComponent],
+      providers: [{
+        provide: IW_TOOLTIP_CONFIG, useValue: config
+      }]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(TooltipContainerComponent);
     component = fixture.componentInstance;
+    component.containerClass = 'container-class-test';
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('adds the containerClass provided as input', () => {
+    expect(fixture.debugElement.classes['container-class-test']).toBe(true);
+  });
+
+  it('adds the containerClass provided in the injected config', () => {
+    expect(fixture.debugElement.classes['class-in-config']).toBe(true);
   });
 });
