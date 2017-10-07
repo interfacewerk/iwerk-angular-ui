f6ba028c162bd42f67a6b1415fafc6e222a2210e
Kevin Merckx <kevin-github@interfacewerk.de>
2017-10-07 18:28:25 +0200

    test(popover.service): add basic tests

--- src/app/ui/popover/popover.service.spec.ts
+++ src/app/ui/popover/popover.service.spec.ts
@@ -1,15 +1,59 @@
-import { TestBed, inject } from '@angular/core/testing';
-
+import { TestBed, tick, fakeAsync } from '@angular/core/testing';
+import { Component, NgModule, ElementRef } from '@angular/core';
 import { PopoverService } from './popover.service';
+import { PopoverContainerComponent } from './popover-container/popover-container.component';
+import { PopoverScrollMaskComponent } from './popover-scroll-mask/popover-scroll-mask.component';
+
+@Component({
+  template: 'my popover test'
+})
+class TestComponent {
+
+}
+
+@Component({
+  template: ''
+})
+class Test2Component {
+  constructor(private popover: PopoverService, private element: ElementRef) {
+
+  }
+
+  open() {
+    return this.popover.open(TestComponent, this.element.nativeElement, {});
+  }
+}
+
+@NgModule({
+  entryComponents: [TestComponent, PopoverContainerComponent, PopoverScrollMaskComponent],
+  declarations: [TestComponent, Test2Component, PopoverContainerComponent, PopoverScrollMaskComponent]
+})
+class TestModule {
+
+}
 
 describe('PopoverService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [PopoverService]
+      providers: [PopoverService],
+      imports: [TestModule],
+      declarations: []
     });
   });
 
-  it('should ...', inject([PopoverService], (service: PopoverService) => {
-    expect(service).toBeTruthy();
+  it('adds the popover container to the body', fakeAsync(() => {
+    const fixture = TestBed.createComponent(Test2Component);
+    const popover = fixture.componentInstance.open();
+    tick(0);
+    expect(document.querySelector('iw-popover-container')).toBeDefined();
+    popover.close();
+  }));
+
+  it('removes the popover container from the body', fakeAsync(() => {
+    const fixture = TestBed.createComponent(Test2Component);
+    const popover = fixture.componentInstance.open();
+    tick(0);
+    popover.close();
+    expect(document.querySelector('iw-popover-container')).toBeNull();
   }));
 });
