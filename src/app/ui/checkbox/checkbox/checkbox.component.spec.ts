1f1433fdc1d69f0d77ee7248a54adb243df697a7
Kevin Merckx <kevin-github@interfacewerk.de>
2018-03-09 09:34:37 +0100

    fix(checkbox): add more tests

--- src/app/ui/checkbox/checkbox/checkbox.component.spec.ts
+++ src/app/ui/checkbox/checkbox/checkbox.component.spec.ts
@@ -23,6 +23,25 @@ describe('CheckboxComponent', () => {
     expect(component).toBeTruthy();
   });
 
+  it('sets the value when calling writeValue', () => {
+    component.writeValue(true);
+    expect(component.value).toBe(true);
+  });
+
+  it('registers onTouched & onChange callbacks which are called onClick', () => {
+    const obj = {
+      touched: () => {},
+      change: () => {}
+    };
+    spyOn(obj, 'touched');
+    spyOn(obj, 'change');
+    component.registerOnTouched(obj.touched);
+    component.registerOnChange(obj.change);
+    component.onClick(undefined);
+    expect(obj.touched).toHaveBeenCalled();
+    expect(obj.change).toHaveBeenCalled();
+  });
+
   it('does not change the value when it is disabled', () => {
     component.onClick(undefined);
     expect(component.value).toBe(true);
