1b2a96a48bfa8b0c4c157a28f2f1ef611cd98280
Kevin Merckx <kevin-github@interfacewerk.de>
2018-03-17 10:38:19 +0100

    style(checkbox): rework styles, simpler, less fancy

--- src/app/demo/checkbox-demo/checkbox-demo.component.ts
+++ src/app/demo/checkbox-demo/checkbox-demo.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-checkbox-demo',
   templateUrl: './checkbox-demo.component.html',
-  styleUrls: ['./checkbox-demo.component.scss']
+  styleUrls: ['./checkbox-demo.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class CheckboxDemoComponent implements OnInit {
 
