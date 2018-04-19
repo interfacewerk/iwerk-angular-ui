e6b336a5b80d4353b85e048706019c9fc3b1be3c
Kevin Merckx <kevin-github@interfacewerk.de>
2018-04-19 08:22:54 +0200

    feat(tooltip): center tooltip horizontally

--- src/app/ui/tooltip/helpers.ts
+++ src/app/ui/tooltip/helpers.ts
@@ -1,20 +1,28 @@
 import { Renderer } from '@angular/core';
 
-export function smartPosition(target: HTMLElement, container: HTMLElement, renderer: Renderer) {
-  const targetRect = target.getBoundingClientRect();
+export function smartPosition(options: {
+  target: HTMLElement,
+  container: HTMLElement,
+  renderer: Renderer
+}) {
+  const targetRect = options.target.getBoundingClientRect();
+  const bodyRect = document.body.getBoundingClientRect();
+  options.container.style.visibility = 'hidden';
 
   // do that after, otherwise it can change the bounding client rect of the target
-  renderer.invokeElementMethod(document.body, 'appendChild', [container]);
+  options.renderer.invokeElementMethod(document.body, 'appendChild', [options.container]);
 
   const y = targetRect.top;
-  const centerYBody = document.body.getBoundingClientRect().height / 2;
+  const centerYBody = bodyRect.height / 2;
   if (y > centerYBody) {
-    container.style.top = (targetRect.top - container.offsetHeight) + 'px';
+    options.container.style.top = (targetRect.top - options.container.offsetHeight) + 'px';
   } else {
-    container.style.top = targetRect.bottom + 'px';
+    options.container.style.top = targetRect.bottom + 'px';
   }
 
-  const maxLeft = document.body.getBoundingClientRect().width - container.offsetWidth;
-  container.style.left = Math.min(maxLeft, targetRect.left) + 'px';
-  container.style.visibility = 'visible';
+  const containerWidth = options.container.offsetWidth;
+  const idealLeft = targetRect.left + targetRect.width / 2  - containerWidth / 2;
+  const maxLeft = bodyRect.width - containerWidth;
+  options.container.style.left = Math.min(maxLeft, idealLeft) + 'px';
+  options.container.style.visibility = 'visible';
 }
