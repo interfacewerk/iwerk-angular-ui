eb6b8ea031a46235aae90c2d5ca923c08f8da7aa
Kevin Merckx <merckx-git@interfacewerk.de>
2019-03-09 10:18:20 +0100

    test(*): fix tests

--- projects/docs/src/lib/core/code-sample/code-sample.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CodeSamplePipe } from './code-sample.pipe';
-
-describe('CodeSamplePipe', () => {
-  it('create an instance', () => {
-    const pipe = new CodeSamplePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
