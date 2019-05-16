2928dfb62ae516f579168a1e95757e02b3e477a3
Kevin Merckx <merckx-git@interfacewerk.de>
2019-05-16 14:25:33 +0200

    chore(move-handle): remove test

--- src/app/ui/movable/move-handle.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MoveHandleDirective } from './move-handle.directive';
-
-describe('MoveHandleDirective', () => {
-  it('should create an instance', () => {
-    const directive = new MoveHandleDirective();
-    expect(directive).toBeTruthy();
-  });
-});
