f9e0b14a8772c096be28e24ed214fdd1e4860956
Kevin Merckx <merckx-git@interfacewerk.de>
2019-05-16 15:02:14 +0200

    chore(movable): fix test

--- src/app/ui/movable/movable.service.spec.ts
+++ src/app/ui/movable/movable.service.spec.ts
@@ -1,12 +1,4 @@
-import { TestBed } from '@angular/core/testing';
 
-import { MovableService } from './movable.service';
 
 describe('MovableService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
-
-  it('should be created', () => {
-    const service: MovableService = TestBed.get(MovableService);
-    expect(service).toBeTruthy();
-  });
 });
