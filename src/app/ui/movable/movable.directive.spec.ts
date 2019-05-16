fb4515f77989a1b03a649bae5045ea72542720fb
Kevin Merckx <merckx-git@interfacewerk.de>
2019-05-16 14:26:55 +0200

    chore(movable): remove test

--- src/app/ui/movable/movable.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MovableDirective } from './movable.directive';
-
-describe('MovableDirective', () => {
-  it('should create an instance', () => {
-    const directive = new MovableDirective();
-    expect(directive).toBeTruthy();
-  });
-});
